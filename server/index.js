348564d7bcf2d71bc9d55cc2b2aa45ab04cd7991
mkawalek <michalkawalekdevkit@icloud.com>
2017-04-24 18:06:41 +0200

    extracted Server from index.js, tests

--- server/index.js
+++ server/index.js
@@ -1,12 +1,8 @@
 import Hull from "hull";
-import bodyParser from "body-parser";
-import cors from "cors";
-
 import express from "express";
-import appMiddleware from "./lib/app-middleware";
-import * as actions from "./actions";
+import Server from "./server";
 
-const { PORT, hostSecret, LOG_LEVEL } = process.env;
+const { PORT = 8082, hostSecret, LOG_LEVEL } = process.env;
 
 if (LOG_LEVEL) {
   Hull.logger.transports.console.level = LOG_LEVEL.LOG_LEVEL;
@@ -17,12 +13,4 @@ const connector = new Hull.Connector({ port: PORT, hostSecret });
 
 connector.setupApp(app);
 
-const middlewareSet = [connector.clientMiddleware(), appMiddleware];
-
-app.post("/fetch", bodyParser.urlencoded({ extended: true }), ...middlewareSet, actions.fetch);
-app.post("/fetch-all", bodyParser.urlencoded({ extended: true }), ...middlewareSet, actions.fetchAll);
-app.get("/admin", ...middlewareSet, actions.admin);
-app.get("/schema/typeforms", cors(), ...middlewareSet, actions.getForms);
-app.get("/schema/questions/:type?", cors(), ...middlewareSet, actions.getQuestions);
-
-connector.startApp(app);
+connector.startApp(Server({ connector, app }));
