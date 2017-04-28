b1bf2319957f327ea27f157c2a0b138a4ecfcb90
Thomas Kgaevski <thomas@hull.io>
2017-04-28 16:40:46 +0200

    fix secret loading from env

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ import Hull from "hull";
 import express from "express";
 import Server from "./server";
 
-const { PORT = 8082, hostSecret, LOG_LEVEL } = process.env;
+const { PORT = 8082, SECRET, LOG_LEVEL } = process.env;
 
 if (LOG_LEVEL) {
-  Hull.logger.transports.console.level = LOG_LEVEL.LOG_LEVEL;
+  Hull.logger.transports.console.level = LOG_LEVEL;
 }
 
 const app = express();
-const connector = new Hull.Connector({ port: PORT, hostSecret });
+const connector = new Hull.Connector({ port: PORT, hostSecret: SECRET });
 
 connector.setupApp(app);
 
